8d439cc899e78ba4d65caf45c25d95031874a3f4
clevernight <clevernight@163.com>
2017-06-15 08:34:37 +0800

    init AgencyDAO

--- src/ts/daoImpl/AgencyDAO.java
+++ src/ts/daoImpl/AgencyDAO.java
@@ -3,9 +3,29 @@ package ts.daoImpl;
 import ts.daoBase.BaseDao;
 import ts.model.Agency;
 
-/**
- * Created by wr on 2017/6/13.
- */
+
 public class AgencyDAO extends BaseDao<Agency,Integer>{
+
+    AgencyDAO() {
+        super(Agency.class);
+    }
+
+    //注册
+    public Agency register(Agency agency) {
+
+        return new Agency();
+    }
+
+    //登陆
+    public Agency login(String phone, String pwd) {
+
+        return new Agency();
+    }
+
+    //修改内容
+    public Agency modify(Agency agency) {
+
+        return new Agency();
+    }
 
 }
